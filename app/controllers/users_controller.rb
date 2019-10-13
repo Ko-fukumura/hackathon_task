17af7ba9b655487e256a2d5cbb5f810243176e2a
福村晃 <fukumurako@fukumuraakiranoMacBook-puro.local>
2019-10-14 04:20:04 +0900

    third commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user, only: [:index, :show, :edit, :update]
+  before_action :forbid_login_user, only: [:new, :create, :login_form, :login]
+  before_action :ensure_correct_user, only: [:edit, :update]
   def index
     @users = User.all
   end
@@ -14,8 +17,10 @@ class UsersController < ApplicationController
   def create
     @user = User.new(name: params[:user][:name],
                       email: params[:user][:email],
+                      password: params[:user][:password],
                       image_name: "default_user.jpg")
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "ユーザー登録が完了しました"
       redirect_to user_url(@user)
     else
@@ -49,4 +54,39 @@ class UsersController < ApplicationController
     flash[:notice] = ""
     redirect_to users_url
   end
+
+  def login_form
+    @user = User.find_by(email: params[:email], password: params[:password])
+  end
+
+  def login
+    @user = User.find_by(email: params[:email], password: params[:password])
+    if @user
+      session[:user_id] = @user.id
+      flash[:notice] = "ログインしました"
+      redirect_to posts_url
+    else
+      @error_message = "メールアドレスまたはパスワードが間違っています"
+      @email = params[:email]
+      @password = params[:password]
+      render 'login_form'
+    end
+  end
+
+  def logout
+    session[:user_id] = nil
+    flash[:notice] = "ログアウトしました"
+    redirect_to login_url
+  end
+
+  private
+
+    def ensure_correct_user
+      if @current_user.id != params[:id].to_i
+        flash[:notice] = "権限がありません"
+        redirect_to posts_url
+      end
+    end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  get '/', to: 'home#top'
+  root 'home#top'
   get 'about', to: 'home#about'
   get 'signup', to: 'users#new'
   post 'signup', to: 'users#create'
+  get 'login', to: 'users#login_form'
+  post 'login', to: 'users#login'
+  post 'logout', to: 'users#logout'
   resources :users
   resources :posts
 end
