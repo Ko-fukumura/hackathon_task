23d04bcc0623f76c59d95ae1b6350be29696c51e
福村晃 <fukumurako@fukumuraakiranoMacBook-puro.local>
2019-10-14 05:26:37 +0900

    fourth commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,14 @@
 class PostsController < ApplicationController
   before_action :authenticate_user
-  
+  before_action :ensure_correct_user, only: [:edit, :update, :destroy]
+
   def index
     @posts = Post.all.order(created_at: :desc)
   end
 
   def show
     @post = Post.find(params[:id])
+    @user = @post.user
   end
 
   def new
@@ -14,7 +16,8 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new(content: params[:post][:content])
+    @post = Post.new(content: params[:post][:content],
+                      user_id: @current_user.id)
     if @post.save
       flash[:notice] = "投稿を作成しました"
       redirect_to posts_url
@@ -43,5 +46,15 @@ class PostsController < ApplicationController
     flash[:notice] = "投稿を削除しました"
     redirect_to posts_url
   end
+
+  private
+
+    def ensure_correct_user
+      @post = Post.find(params[:id])
+      if @post.user_id != @current_user.id
+        flash[:notice] = "権限がありません"
+        redirect_to posts_url
+      end
+    end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_13_070842) do
+ActiveRecord::Schema.define(version: 2019_10_13_192421) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
